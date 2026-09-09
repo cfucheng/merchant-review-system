5ed115055279a4c4aac1c2a7fe8c6c7106643e84
ccfucheng <2303734361@qq.com>
2026-09-09 20:08:00 +0800

    解决状态登录刷新问题

--- src/main/java/com/hmdp/config/MvcConfig.java
+++ src/main/java/com/hmdp/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.hmdp.config;
 
 import com.hmdp.interceptor.LoginInterceptor;
+import com.hmdp.interceptor.RefreshTokenInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -20,8 +21,14 @@ public class MvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginInterceptor loginInterceptor;
 
+    @Autowired
+    private RefreshTokenInterceptor refreshTokenInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(refreshTokenInterceptor)
+                .addPathPatterns("/**")
+                .order(0);
         registry.addInterceptor(loginInterceptor)
                 .excludePathPatterns(
                         "/shop/**",
@@ -31,6 +38,7 @@ public class MvcConfig implements WebMvcConfigurer {
                         "/blog/hot",
                         "/user/code",
                         "/user/login"
-                );
+                )
+                .order(1);
     }
 }
--- src/main/java/com/hmdp/interceptor/LoginInterceptor.java
+++ src/main/java/com/hmdp/interceptor/LoginInterceptor.java
@@ -1,23 +1,10 @@
 package com.hmdp.interceptor;
-
-import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.core.util.StrUtil;
-import com.hmdp.config.TokenProperties;
-import com.hmdp.dto.UserDTO;
-import com.hmdp.entity.User;
-import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.UserHolder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 /**
  *@Author: chengfu Chen
@@ -27,46 +14,16 @@ import java.util.concurrent.TimeUnit;
  */
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
-    @Autowired
-    private StringRedisTemplate stringRedisTemplate;
-    @Autowired
-    private TokenProperties tokenProperties;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 1.从请求头中获取token
-        String token = request.getHeader(tokenProperties.getHeaderName());
-        if (StrUtil.isBlank(token)) {//判断是否为空
-            // 不存在 拦截 返回401
+        // 1.判断是否需要拦截(ThreadLocal中是否存在用户)
+        if (UserHolder.getUser() == null) {
+            // 没有, 需要拦截, 设置状态码
             response.setStatus(401);
             return false;
         }
-        // 2.基于token获取用户信息
-        String key = tokenProperties.getKeyPrefix() + token;
-        Map<Object, Object> userMap = stringRedisTemplate.opsForHash().entries(key);
-
-        // 3.判断用户是否存在
-        if (userMap.isEmpty()){
-            // 不存在 拦截 返回401
-            response.setStatus(401);
-            return false;
-        }
-
-        // 4.将查询到的Hash数据转换为UserDTO对象
-        UserDTO userDTO = BeanUtil.toBean(userMap, UserDTO.class);
-
-        // 5.存在 保存信息到ThreadLocal
-        UserHolder.saveUser(userDTO);
-
-        // 6.刷新有效期
-        stringRedisTemplate.expire(key, tokenProperties.getTtlMinutes(), TimeUnit.MINUTES);
-
-        // 7. 放行
+        // 有用户 放行
         return true;
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        UserHolder.removeUser();
     }
 }
