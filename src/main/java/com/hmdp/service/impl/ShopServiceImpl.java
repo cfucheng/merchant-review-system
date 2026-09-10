603cb6de4dedd07b0776b797f0c0697a7c9875bd
ccfucheng <2303734361@qq.com>
2026-09-10 10:30:15 +0800

    添加商户缓存

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,10 +1,22 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -17,4 +29,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 根据id查询商铺信息
+     * @param id 商铺id
+     * @return 商铺详情数据
+     */
+    public Result queryById(Long id) {
+        // 1.在redis中查询商铺信息
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        String cacheShop = stringRedisTemplate.opsForValue().get(key);
+        // 2.存在 直接返回
+        if (StrUtil.isNotBlank(cacheShop)) {
+            return Result.ok(JSONUtil.toBean(cacheShop, Shop.class));
+        }
+        // 3.不存在，根据id查询数据库
+        Shop shop = getById(id);
+        // 4.数据库不存在，返回错误
+        if (shop == null) {
+            return Result.fail("店铺不存在");
+        }
+        // 5.数据库存在，写入redis并返回
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
+        // 6.返回
+        return Result.ok(shop);
+    }
 }
